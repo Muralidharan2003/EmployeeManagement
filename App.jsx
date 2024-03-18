de92a443ea603b8af0647ba00bf930db5c64e073
Muralidharan2003 <109496904+Muralidharan2003@users.noreply.github.com>
2024-03-18 11:25:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/Login'
+import Home from './components/Home'
+import Employee from './components/Employee'
+import Category from './components/Category'
+import Profile from './components/Profile'
+import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import AddCategory from './components/AddCategory'
+import AddEmployee from './components/AddEmployee'
+import EditEmployee from './components/EditEmployee'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/adminlogin' element={<Login />}></Route>
+    <Route path='/dashboard' element={<Dashboard/>}>
+    <Route path='' element={<Home/>}></Route>
+    <Route path='/dashboard/employee' element={<Employee/>}></Route>
+    <Route path='/dashboard/category' element={<Category />}></Route>
+    <Route path='/dashboard/profile' element={<Profile />}></Route>
+    <Route path='/dashboard/add_category' element={<AddCategory/>}></Route>
+    <Route path='/dashboard/add_employee' element={<AddEmployee/>}></Route>
+    <Route path='/dashboard/edit_employee/:id' element={<EditEmployee/>}></Route>
+    </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
